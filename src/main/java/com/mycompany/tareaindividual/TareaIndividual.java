a288913114046741c370637b65e9d25de83ec365
jorgeJimz <127570326+jorgeJimz@users.noreply.github.com>
2023-03-10 20:19:11 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/tareaindividual/TareaIndividual.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareaindividual;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TareaIndividual {
+
+    public static void main(String[] args) {
+     
+       		
+		System.out.println("Hola, Soy de la clase de Programación Orientada a Objetos");
+	}
+        
+}
+    
